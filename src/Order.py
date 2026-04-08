eac3d2e1e5ee2a0340766baf601ae9f963bd7589
Roland Eschenburg <roland.eschenburg@hotmail.co.uk>
2026-04-08 22:33:37 +0200

    adding product to  order item list

--- src/Order.py
+++ src/Order.py
@@ -1,4 +1,7 @@
 class Order():
+    def __init__(self):
+        self.items = []
 
     def add_item(self, product, quantity):
         product.hold += 1
+        self.items.append({product: quantity})
