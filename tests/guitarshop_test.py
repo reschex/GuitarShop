0ff5be26e286eee912094fb1c5919f17a65e0416
Roland Eschenburg <roland.eschenburg@hotmail.co.uk>
2026-04-08 21:56:45 +0200

    hold increases by one when item added to order

--- tests/guitarshop_test.py
+++ tests/guitarshop_test.py
@@ -1,3 +1,24 @@
+class Product():
+    def __init__(self, id, stock, hold):
+        self.id = id
+        self.stock = stock
+        self.hold = hold
+
+
+class Order():
+    def __init__(self):
+        pass
+
+    def add_item(self, product, quantity):
+        product.hold += 1
+
+
 class TestGuitarShop():
     def test_basic(self):
         assert 1 == 1
+
+    def test_hold_is_placed_when_product_added_to_an_order(self):
+        product = Product(327, 7, 0)
+        order = Order()
+        order.add_item(product, 1)
+        assert product.hold == 1
